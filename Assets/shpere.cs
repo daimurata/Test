fba069ff488697b0d7b3c3f9f14dfbb93cf3345a
daimurata <nkc.dai.murata@gmail.com>
2016-12-20 12:50:09 +0900

    Test

---
+++ Assets/shpere.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shpere : MonoBehaviour {
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        Vector3 pos = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Fire1（左クリックまたは左Ctrlキー）が押されたときの処理.
+            pos.z = 1.0f;
+        }
+        transform.position += pos;
+        
+    }
+}
